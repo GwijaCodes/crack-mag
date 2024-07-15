51ae2787720210d1162ae654efdd0f2d45a9f0c7
GLORIA SAIJA <gloriasaija2000@gmail.com>
2024-07-15 19:03:13 +0200

    styled menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,33 +4,30 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
-  // State to manage the navbar's visibility
   const [nav, setNav] = useState(false);
 
-  // Toggle function to handle the navbar's display
   const handleNav = () => {
     setNav(!nav);
   };
 
   const navItems = [
-    { id: 1, text: 'Home', link: "/" },
-    { id: 2, text: 'Magazine', link: '/magazine' },
-    { id: 3, text: 'Articoli', link: '/articoli' },
-    { id: 4, text: 'About Crack!', link: '/aboutcrack' },
-    { id: 5, text: 'Chi siamo', link: '/aboutus' },
+    { id: 2, text: 'Magazine', link: './magazine' },
+    { id: 3, text: 'Articoli', link: './articoli' },
+    { id: 4, text: 'About Crack!', link: './aboutcrack' },
+    { id: 5, text: 'Chi siamo', link: './aboutus' },
   ];
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
-      <h1 className='title w-full text-3xl font-bold'>Crack!</h1>
-      <button type="button" className="focus:outline-none font-medium text-sm px-4 py-2 text-center">O</button>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-[20%] gap-2'>
+      <a href='/' className='title w-full text-3xl font-bold'>Crack!</a>
+      <button type="button" className="button rounded font-medium text-sm px-4 py-2 text-center">O</button>
       {/* Desktop Navigation */}
       <ul className='hidden md:flex'>
         {navItems.map(item => (
           <Link
             key={item.id}
-            href={item.link}
             className='p-4 m-2 cursor-pointer duration-300'
+            href={item.link}
           >
             {item.text}
           </Link>
@@ -39,26 +36,32 @@ const Navbar = () => {
 
       {/* Mobile Navigation Icon */}
       <div onClick={handleNav} className='block md:hidden'>
-        {nav ? <i>X</i> : <i>!!!</i>}
+        {nav ? <i className='button mx-[10px]'>X</i> : <i className='button mx-[10px]'>=</i>}
       </div>
 
       {/* Mobile Navigation Menu */}
+      
       <ul
         className={
           nav
-            ? 'fixed md:hidden left-0 top-0 w-[60%] ease-in-out duration-500 bg-white'
-            : 'ease-in-out w-[60%] duration-500 fixed bottom-0 left-[-100%]'
+            ? 'fixed bg-[#FF98F8] h-[50v] border-black border-2 md:hidden left-0 bottom-0 w-screen py-4 px-[20%] ease-in-out duration-500'
+            : 'ease-in-out duration-500 fixed w-screen bottom-[-200%] left-0'
         }
       >
-
+        <h2 className='title text-2xl'>Menu</h2>
         {/* Mobile Navigation Items */}
         {navItems.map(item => (
+          <Link
+          href={item.link}
+          >
           <li
             key={item.id}
-            className='p-4 duration-300 cursor-pointer'
+            
+            className='button p-4 duration-300 cursor-pointer title text-sm'
           >
             {item.text}
           </li>
+          </Link>
         ))}
       </ul>
     </div>
